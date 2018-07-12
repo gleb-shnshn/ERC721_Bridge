c169d73f56d69c42bdc8da9ae12d38c0d8f5ba68
gleb270 <32265172+gleb270@users.noreply.github.com>
2018-07-12 12:29:57 +1000

    comments

--- SmartPhone.sol
+++ SmartPhone.sol
@@ -3,47 +3,48 @@ pragma solidity ^0.4.24;
 contract SmartPhone {
 
     struct Phone {
-        uint weight;
-        bool demolished;
-        string color;
-        address owner;
-        bytes32 id;
+        uint weight;//weight of the phone
+        bool demolished;//is the phone demolished
+        string color;//color of the phone
+        address owner;//owner of the phone
+        bytes32 id;//unique id of the phone
     }
     
-    address dev;
+    address dev;//address of person who deployed contract, he has permission of registering vendors
     
     mapping(address=>bool) isVendor;
     
-    Phone[] phones;
+    Phone[] phones;//array of phones
     
-    function SmartPhone() public{
-        isVendor[msg.sender]=true;
-        dev=msg.sender;
+    function SmartPhone() public{//constructor of the contract
+        isVendor[msg.sender]=true;//giving permission to create phones for person who deployed contract
+        dev=msg.sender;//initializing dev
     }
     
-    modifier valid(bytes32 _id){
-        require(msg.sender==phones[getPhone[_id]].owner);
-        require(phones[getPhone[_id]].demolished==false);
+    modifier valid(bytes32 _id){//modifier of existing of the phone with gived id
+        require(msg.sender==phones[getPhone[_id]].owner);//if sender is an owner of the phone 
+        require(phones[getPhone[_id]].demolished==false);//if phone is not demolished
         _;
     }
     
-    mapping(bytes32=>uint) getPhone;
+    mapping(bytes32=>uint) getPhone;//get index of the phone in array of phones by id
     
-    function registerVendor(address _vendor) public{
-        require(dev==msg.sender);
-        isVendor[_vendor]=true;
+    function registerVendor(address _vendor) public{//registering a new vendor
+        require(dev==msg.sender);//if sender is dev
+        isVendor[_vendor]=true;//giving permisson to create phones for a new vendor
     }
     
-    function transfer(address _to, bytes32 _id) public valid(_id){
+    function transfer(address _to, bytes32 _id) public valid(_id){//transfering the phone to the new owner if the phone is existing
         phones[getPhone[_id]].owner=_to;
     }
     
     function createPhone(uint _weight, string _color) public{
-        require(isVendor[msg.sender]);
+        require(isVendor[msg.sender]);//if sender is a vendor
         phones.push(Phone(_weight,false,_color,msg.sender,keccak256(_color,_weight, phones.length)));
+        //generating unique id via keccak256
     }
     
-    function demolish(bytes32 _id) public valid(_id){
+    function demolish(bytes32 _id) public valid(_id){//demolishing the phone if the phone is existing
         phones[getPhone[_id]].demolished=true;
     }
 }
