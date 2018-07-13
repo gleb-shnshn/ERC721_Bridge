871d2442cd6156b5c9675043e4d8f2c3d6858d78
gleb270 <32265172+gleb270@users.noreply.github.com>
2018-07-14 00:49:56 +1000

    Update SmartPhone.sol

--- SmartPhone.sol
+++ SmartPhone.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 contract ERC721{
-    function onERC721Received(address _operator, address _from, bytes32 _tokenId, bytes data) external returns(bytes4);
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes data) external returns(bytes4);
 }
 contract SmartPhone {
 
@@ -9,12 +9,14 @@ contract SmartPhone {
         bool demolished;//is the phone demolished
         string color;//color of the phone
         address owner;//owner of the phone
-        bytes32 tokenId;//unique id of the phone
+        uint256 tokenId;//unique id of the phone
     }
     
     address dev;//address of person who deployed contract, he has the permission of registering vendors
     
     mapping(address=>bool) isVendor;
+    
+    mapping(address=>uint256) getBalance;
     
     Phone[] phones;//array of phones
     
@@ -23,45 +25,66 @@ contract SmartPhone {
         dev=msg.sender;//initializing dev
     }
     
-    mapping(bytes32=>uint) getPhone;//get index of the phone in array of phones by id
-    
     function registerVendor(address _vendor) public{//registering a new vendor
         require(dev==msg.sender);//if sender is dev
         isVendor[_vendor]=true;//giving permisson of creating phones for a new vendor
     }
     
-    function isContract(address addr) returns (bool) {//checking if addr is a contract
+    function isContract(address addr) public view returns (bool) {//checking if addr is a contract
         uint size;
         assembly { size := extcodesize(addr) }
         return size > 0;
     }
     
-    function onERC721Received(address _operator, address _from, bytes32 _tokenId, bytes data) external returns(bytes4){
-       return( bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")));
+    function getColor(uint256 _tokenId) public view returns(string){
+        require(phones[_tokenId].tokenId!=0);
+        return(phones[_tokenId].color);
     }
     
-    function safeTransferFrom(address _from, address _to, bytes32 _tokenId) public payable{//transfering the phone to the new owner if the phone is existing
+     function getWeight(uint256 _tokenId) public view returns(uint){
+        require(phones[_tokenId].tokenId!=0);
+        return(phones[_tokenId].weight);
+    }
+    
+     function ownerOf(uint256 _tokenId) public view returns(address){
+        require(phones[_tokenId].tokenId!=0);
+        return(phones[_tokenId].owner);
+    }
+    
+    function balanceOf(address _owner) public view returns(uint256){
+        return(getBalance[_owner]);
+    }
+    
+    event sent();
+    event newToken(uint256);
+    
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes data) external returns(bytes4){
+         return( bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")));
+    }
+    
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public payable{//transfering the phone to the new owner if the phone is existing
         require(_from==msg.sender);//if sender is an owner of address _from
-        require(_from==phones[getPhone[_tokenId]].owner);//if sender is an owner of the phone 
-        require(_tokenId==phones[getPhone[_tokenId]].tokenId);//if token exists
-        require(phones[getPhone[_tokenId]].demolished==false);//if phone is not demolished
-        phones[getPhone[_tokenId]].owner=_to;//changing owner
+        require(_from==phones[_tokenId].owner);//if sender is an owner of the phone
+        require(phones[_tokenId].demolished==false);//if phone is not demolished
+        phones[_tokenId].owner=_to;//changing owner
+        getBalance[_from]--;
+        getBalance[_to]++;
         if (isContract(_to)){
             ERC721 erc721 = ERC721(_to);
             require(erc721.onERC721Received(_from,_from, _tokenId, "")==bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")));
         }
+        emit sent();//emit event of successful sending money
     }
     
     function createPhone(uint _weight, string _color) public{
         require(isVendor[msg.sender]);//if sender is a vendor
-        bytes32 _tokenId = keccak256(_color,_weight, phones.length);//generating unique id via keccak256
+        uint256 _tokenId = phones.length;//a token id is a index in array
         phones.push(Phone(_weight,false,_color,msg.sender,_tokenId));//adding new phone in array
-        getPhone[_tokenId]=phones.length-2;
     }
     
-    function demolish(bytes32 _tokenId) public{//demolishing the phone if the phone is existing
-        require(msg.sender==phones[getPhone[_tokenId]].owner);//if sender is an owner of the phone 
-        require(phones[getPhone[_tokenId]].demolished==false);//if phone is not demolished
-        phones[getPhone[_tokenId]].demolished=true;
+    function demolish(uint256 _tokenId) public{//demolishing the phone if the phone is existing
+        require(msg.sender==phones[_tokenId].owner);//if sender is an owner of the phone 
+        require(phones[_tokenId].demolished==false);//if phone is not demolished
+        phones[_tokenId].demolished=true;
     }
 }
