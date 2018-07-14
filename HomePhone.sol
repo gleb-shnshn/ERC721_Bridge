a14b8da54dc827fb4d9431c9683c3d541dc3fe9f
Gleb <32265172+gleb270@users.noreply.github.com>
2018-07-15 09:54:53 +1000

    Update and rename SmartPhone.sol to HomePhone.sol

--- HomePhone.sol
+++ HomePhone.sol
@@ -4,6 +4,11 @@ pragma experimental ABIEncoderV2;
 contract ERC721{
     function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes data) external returns(bytes4);
 }
+
+contract Recovery{
+    function recovery(uint256 _tokenId, bytes[] _data) public;
+}
+
 contract SmartPhone {
 
     struct Phone {
@@ -30,7 +35,7 @@ contract SmartPhone {
         data[5]=abi.encodePacked(_phone.brand);
         return (data);
     }
-    
+
     address dev;//address of person who deployed contract, he has the permission of registering vendors
     
     mapping(address=>bool) isVendor;//true if address is a vendor
@@ -41,6 +46,39 @@ contract SmartPhone {
     
     Phone[] phones;//array of phones
     
+    function recoveryToken(uint256 _tokenId, bytes[] _data) public{
+        Phone storage _phone;
+        _phone.weight=(bytesToUint(_data[0]));
+        if (bytesToUint(_data[1])==1)
+            _phone.demolished=true;
+        else
+            _phone.demolished=false;
+        _phone.color=string(_data[2]);
+        _phone.owner=bytesToAddr(_data[3]);
+        _phone.tokenId=_tokenId;
+        _phone.model=string(_data[4]);
+        _phone.brand=string(_data[5]);
+        emit phoneRecovered(_phone.weight, _phone.demolished, _phone.color,_phone.owner, _phone.tokenId, _phone.model, _phone.brand);
+        phones.push(_phone);
+    }
+    
+    function bytesToUint(bytes b) public returns (uint256){
+        uint256 number;
+        for(uint i=0;i<b.length;i++){
+            number = number + uint(b[i])*(2**(8*(b.length-(i+1))));
+        }
+        return number;
+    } 
+    
+    function bytesToAddr (bytes b) constant returns (address) {
+        uint result = 0;
+        for (uint i = b.length-1; i+1 > 0; i--) {
+            uint c = uint(b[i]);
+            uint to_inc = c * ( 16 ** ((b.length - i-1) * 2));
+            result += to_inc;
+        }
+        return address(result); 
+    }
     function tokenByIndex(uint256 _index) external view returns (uint256){//return token id of phone with given index
         require(_index<phones.length);
         require(_index>=0);
@@ -122,7 +160,7 @@ contract SmartPhone {
     function createPhone(string _model, uint _weight, string _color) public{//creating a new phone
         require(isVendor[msg.sender]);//if sender is a vendor
         uint256 _tokenId = phones.length+1;//a token id is a index in array
-        phones.push(Phone(_weight,false,_color,msg.sender,_tokenId,_model,getName[msg.sender]));//adding new phone in array'
+        phones.push(Phone(_weight,false,_color,msg.sender,_tokenId,_model,getName[msg.sender]));//adding new phone in array
         getBalance[msg.sender]++;//changing the balance of the vendor
     }
     
