87406e0e82ea9e8a9e5d1c373ec9eb1e6fdcf0ca
Gleb <32265172+gleb270@users.noreply.github.com>
2018-07-15 12:02:08 +1000

    Update and rename HomeBridge to HomeBridge.sol

--- HomeBridge.sol
+++ HomeBridge.sol
@@ -3,8 +3,16 @@ pragma experimental ABIEncoderV2;
 
 contract HomePhone{
     function recoveryToken(uint256 _tokenId, bytes[] _data) public;
+    function getSerializedData(uint256 _tokenId) public view returns(bytes[]);
 }
 contract HomeBridge{
+    
+    event CametoBridge(address reciever, uint256 tokenId, bytes[] data);
+    
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes data) external returns(bytes4){
+         emit CametoBridge(_from, _tokenId, homePhone.getSerializedData(_tokenId));
+         return( bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")));
+    }
     
     HomePhone homePhone;
     
