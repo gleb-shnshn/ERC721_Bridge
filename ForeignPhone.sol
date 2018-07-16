fea403807a3ac86947e47c31800156adec3f6c01
Gleb <32265172+gleb270@users.noreply.github.com>
2018-07-16 15:17:53 +1000

    Update ForeignPhone.sol

--- ForeignPhone.sol
+++ ForeignPhone.sol
@@ -7,6 +7,8 @@ contract ERC721{
 
 contract ForeignPhone {
 
+    bool canCreate=false;
+
     struct Phone {
         uint weight;//weight of the phone
         bool demolished;//is the phone demolished
@@ -32,27 +34,29 @@ contract ForeignPhone {
         return (data);
     }
 
+    address dev;//address of person who deployed contract, he has the permission of registering vendors
+    
+    mapping(address=>bool) isVendor;//true if address is a vendor
+    
     mapping(address=>uint256) getBalance;//return count of tokens for the address
+    
+    mapping(address=>string) getName;//return the name of a vendor
     
     event phoneRecovered(uint, bool, string, address, uint256, string, string);
 
-    mapping(uint=>Phone) phones;//array of phones
+    mapping(uint=>Phone) phones;//map of phones
     
-    address foreignBridge;
+    uint total;
     
-    function setForeignBridge(address _foreignBridgeContract) public{
+    address Bridge;
+    
+    function setBridge(address _bridgeContract) public{
         require(msg.sender==dev);
-        foreignBridge=_foreignBridgeContract;
+        Bridge=_bridgeContract;
     }
-    
-    address dev;
-    
-    constructor(){
-        dev=msg.sender;
-    }
-    
+
     function recoveryToken(uint256 _tokenId, bytes[] _data) public{
-        require(msg.sender==foreignBridge);
+        require(msg.sender==Bridge);
         Phone storage _phone;
         _phone.weight=(bytesToUint(_data[0]));
         if (bytesToUint(_data[1])==1)
@@ -66,6 +70,8 @@ contract ForeignPhone {
         _phone.brand=string(_data[5]);
         emit phoneRecovered(_phone.weight, _phone.demolished, _phone.color,_phone.owner, _phone.tokenId, _phone.model, _phone.brand);
         phones[_tokenId-1]=_phone;
+        getBalance[_phone.owner]++;
+        total++;
     }
     
     function bytesToUint(bytes b) public returns (uint256){
@@ -86,10 +92,37 @@ contract ForeignPhone {
         return address(result); 
     }
     
+    function tokenByIndex(uint256 _index) external view returns (uint256){//return token id of phone with given index
+        require(_index<total);
+        require(_index>=0);
+        return phones[_index+1].tokenId;
+    }
+    
+    function totalSupply() external view returns (uint256){//return count of all tokens
+        return total;
+    }
+    
+    constructor() public{//constructor of the contract
+        isVendor[msg.sender]=true;//giving permission of creating phones for person who deployed contract
+        dev=msg.sender;//initializing dev
+        getName[msg.sender]="Developer";
+    }
+    
+    function registerVendor(address _vendor, string _name) public{//registering a new vendor
+        require(dev==msg.sender);//if sender is dev
+        getName[_vendor]=_name;//giving the name for the vendor
+        isVendor[_vendor]=true;//giving permisson of creating phones for a new vendor
+    }
+    
     function isContract(address addr) public view returns (bool) {//checking if addr is a contract
         uint size;
         assembly { size := extcodesize(addr) }
         return size > 0;
+    }
+    
+    function changeVendorName(string _newName) public{
+        require(isVendor[msg.sender]==true);//if sender is a vemdor
+        getName[msg.sender]=_newName;//rename the  vendor
     }
     
     function getColor(uint256 _tokenId) public view returns(string){//returning the color of the phone
@@ -136,8 +169,26 @@ contract ForeignPhone {
         emit sent();//emit event of successful sending money
     }
     
-    function deleteToken(uint256 _tokenId){
-        require(dev==foreignBridge);
+    function createPhone(string _model, uint _weight, string _color) public{//creating a new phone
+        require(canCreate);
+        require(isVendor[msg.sender]);//if sender is a vendor
+        uint256 _tokenId = total+1;//a token id is a index in array
+        phones[_tokenId]=(Phone(_weight,false,_color,msg.sender,_tokenId,_model,getName[msg.sender]));//adding new phone in array'
+        getBalance[msg.sender]++;//changing the balance of the vendor
+    }
+    
+    function demolish(uint256 _tokenId) public{//demolishing the phone if the phone is existing
+        require(canCreate);
+        require(msg.sender==phones[_tokenId-1].owner);//if sender is an owner of the phone 
+        require(phones[_tokenId-1].demolished==false);//if phone is not demolished
+        phones[_tokenId-1].demolished=true;
+    }
+    
+    function deleteToken(uint256 _tokenId) public{
+        require(!canCreate);
+        require(dev==Bridge);
         delete phones[_tokenId-1];
+        total--;
+        getBalance[phones[_tokenId-1].owner]--;
     }
 }
