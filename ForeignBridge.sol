9b751e359e1f8028929ac73a68984ba4719ebc28
Gleb <32265172+gleb270@users.noreply.github.com>
2018-07-16 12:03:24 +1000

    Update ForeignBridge.sol

--- ForeignBridge.sol
+++ ForeignBridge.sol
@@ -16,12 +16,47 @@ contract ForeignBridge{
          return(bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")));
     }
     
+    mapping(address => bool) isExtBridge;
+    uint requiredSig;
+    
     ForeignPhone foreignPhone;
+    address dev;
+    
+    mapping(bytes32=>uint) signsOf;
+    mapping(bytes32=>bool) tokenRecovered;
+    mapping(bytes32=>bool) isVoted;
     
     event TransferCompleted(uint256);
     
-    constructor(address _foreignPhoneContract) public{
+    constructor(address _foreignPhoneContract, address[] _extBridges, uint _requiredSig) public{
+        for(uint i=0; i<_extBridges.length; i++){
+            isExtBridge[_extBridges[i]]=true;
+        }
+        requiredSig=_requiredSig;
         foreignPhone=ForeignPhone(_foreignPhoneContract);
+        dev=msg.sender;
+    }
+    
+     function preTrasfer(bytes32 _txHash, uint256 _tokenId, bytes[] _data, address _reciever) public{
+        require(isExtBridge[msg.sender]==true);
+        bytes memory __data;
+        for (uint i=0; i<_data.length;i++){
+            __data=abi.encodePacked(__data,_data[i]);
+        }
+        bytes32 _hash=keccak256(_reciever, _tokenId, __data, _txHash);
+        require(isVoted[keccak256(_hash,msg.sender)]==false);
+        isVoted[keccak256(_hash,msg.sender)]=true;
+        signsOf[_hash]++;
+        if (signsOf[_hash]>=requiredSig && tokenRecovered[_hash]==false){
+            transferApproved(_tokenId, _data);
+            tokenRecovered[_hash]=true;
+        }
+        
+    }
+    
+    function addExtBridge(address _extBridge){
+        require(msg.sender==dev);
+        isExtBridge[_extBridge]=true;
     }
     
     function transferApproved(uint256 _tokenId, bytes[] _data) public{
